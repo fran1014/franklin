e38c82f9b75a24b6ad82b5482c206daa4d272d0a
Franklin <franklin1014.op@gmail.com>
2023-11-10 12:12:11 +0100

    First Commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
 
 //@description Fetch all products
 //@route Get api/products
@@ -21,8 +21,29 @@ const getProductsById = asyncHandler(async (req, res) => {
     return res.json(product);
   } else {
     res.status(404);
-    throw new Error("Resource not found");
+    throw new Error('Resource not found');
   }
 });
 
-export { getProducts, getProductsById };
+//@description Create a product
+//@route Post api/products
+//@access Private/Admin
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: 'images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createProduct = await product.save();
+  res.status(201).json(createProduct);
+});
+
+export { getProducts, getProductsById, createProduct };
