9f88d729a6a11ec1d9c0b28c90f761561c79169d
Franklin <franklin1014.op@gmail.com>
2023-11-19 16:30:04 +0100

    update files

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+  createProduct,
+  getProducts,
+  getProductsById,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id').get(getProductsById);
+
+export default router;
