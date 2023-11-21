54240ee481cf20d854de69cebd522003340029a9
Franklin <franklin.op@hotmail.com>
2023-11-21 14:03:19 +0100

    updated paid route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -14,6 +14,6 @@ const router = express.Router();
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/mine').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
-router.route('/:id/pay').put(protect, admin, updateOrderToPaid);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 export default router;
